ca7cc464ebb06b42a5d4cde44aacbaf2dd8ee601
quijano <safiro.quijano@nokia.com>
2020-04-10 23:46:57 +0800

    create an instance of a mongoose model and save it to mongodb via its save() method

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,3 +5,26 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useUnifiedTopology:true,
     useCreateIndex: true
 });
+
+//create mongoose model for user with {name,age}
+const User = mongoose.model('User', {
+    name:{
+        type: String
+    },
+    age: {
+        type: Number
+    }
+});
+
+//create User instance jiro
+const jiro = new User({
+    name: 'jiro',
+    age: 28
+});
+
+// commit/save it to mongodb
+jiro.save().then((data)=>{
+    console.log(data);
+}).catch((error)=>{
+    console.log(error)
+});
