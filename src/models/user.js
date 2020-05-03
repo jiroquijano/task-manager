7e99268c97283ba8e6d25b2189b55ad6bc8455bf
quijano <safiro.quijano@nokia.com>
2020-05-03 20:45:00 +0800

    added query option in get tasks route and added timestamps in user and task schemas

--- src/models/user.js
+++ src/models/user.js
@@ -48,6 +48,8 @@ const userSchema = new mongoose.Schema({
             required: true
         }
     }]
+}, {
+    timestamps: true
 });
 
 userSchema.virtual('tasks',{ //not saved in MongoDB but serves as a reference to Mongoose about schema relations
